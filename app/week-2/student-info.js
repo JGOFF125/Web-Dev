f49a4755d6247b6123b48230508ff079996caef3
JG_OFF <Joaquinurbanof@gmail.com>
2026-09-18 02:20:39 -0600

    Complete Week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <section>
+      <p>Joaquin Urbano</p>
+      <Link href="https://github.com/JGOFF125/Web-Dev">
+        My GitHub Repository
+      </Link>
+    </section>
+  );
+}
